01b5b30b860df5f6565fe16277df8fd5cb39a8e1
Devansh Gulhane <devagul93@gmail.com>
2019-06-22 13:38:15 +0530

    Added react-native-firebase

--- packages/mobile/android/app/src/main/java/com/reactnativemonorepostarter/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/reactnativemonorepostarter/MainApplication.java
@@ -12,6 +12,8 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import io.invertase.firebase.RNFirebasePackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -24,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RNGestureHandlerPackage()
+          new RNGestureHandlerPackage(),
+              new RNFirebasePackage()
       );
     }
 
---
+++ packages/mobile/ios/Pods/Target Support Files/Pods-reactNativeMonoRepoStarter/Pods-reactNativeMonoRepoStarter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativeMonoRepoStarter : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativeMonoRepoStarter
+@end
